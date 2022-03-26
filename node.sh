c251b69347a9ac21dfd9bd2aa32130558aeb7c40
MasterTeddy-AU <96571125+MasterTeddy-AU@users.noreply.github.com>
2022-03-26 17:34:52 +1030

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+if [[ "$EUID" -ne 0 ]]; then
+	echo "This installer needs to be run with superuser privileges."
+	exit
+fi
+
+#-- Clean up installation mess -------------------------------------------------------------------#
+USER="$(grep "user-setup: Adding user" /var/log/installer/syslog|head -n1|awk '{print $7}'|sed "s/[\'\`]//g")"
+userdel -rf ${USER}
+
+#-- Prepare system -------------------------------------------------------------------------------#
+apt -y purge openssh*
+
+apt-get -yq update;
+apt-get -yq upgrade;
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common sudo -qy;
+apt -qy install sysvinit-core sysvinit-utils inetutils-syslogd dropbear;
+
+	# Enable net.ipv4.ip_forward for the system
+		echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-route48-forward.conf
+	# Enable without waiting for a reboot or service restart
+		echo 1 > /proc/sys/net/ipv4/ip_forward
+	# Enable net.ipv6.conf.all.forwarding for the system
+		echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.d/30-route48-forward.conf
+	# Enable without waiting for a reboot or service restart
+		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+	
+apt-get update -yq && \
+apt-get -yq install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  htop \
+  iftop \
+  vnstat \
+  sudo \
+  vnstat \
+  curl \
+  git \
+  nano \
+  wget \
+  bird \
+  snmpd \
+  apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/*
+  
+ cd /opt/;
+ /usr/sbin/git clone https://github.com/Cloudie-Networks/R48.git
+ 
+ mkdir -p /root/.ssh;
+ cat /opt/R48/root/root/.ssh/authorized_keys >> /root/.ssh/authorized_keys;
+ chmod 0600 /root/.ssh
+ chmod 0600 /root/.ssh/authorized_keys
+ 
+ mv /opt/R48/root/root/opt /opt/;
+ rm -rf /opt/R48;
+ 
+ 
+cat > /etc/default/dropbear << "EOF"
+# the TCP port that Dropbear listens on
+DROPBEAR_PORT=9092
+
+# any additional arguments for Dropbear
+DROPBEAR_EXTRA_ARGS=-s
+
+# specify an optional banner file containing a message to be
+# sent to clients before they connect, such as "/etc/issue.net"
+DROPBEAR_BANNER=""
+
+# RSA hostkey file (default: /etc/dropbear/dropbear_rsa_host_key)
+#DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
+
+# DSS hostkey file (default: /etc/dropbear/dropbear_dss_host_key)
+#DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
+
+# ECDSA hostkey file (default: /etc/dropbear/dropbear_ecdsa_host_key)
+#DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
+
+# Receive window size - this is a tradeoff between memory and
+# network performance
+DROPBEAR_RECEIVE_WINDOW=65536
+EOF
+
+cat > /etc/rc.local << "EOF"
+#!/bin/sh
+[ -d /etc/boot.d ] && run-parts /etc/boot.d
+exit 0
+EOF
+chmod +x /etc/rc.local
+mkdir -p /etc/boot.d
+cat > /etc/boot.d/udev << "EOF"
+#!/bin/sh
+service udev stop
+EOF
+chmod +x /etc/boot.d/udev
+
+cat > /etc/boot.d/autoclean << "EOF"
+#!/bin/sh
+for pkg in rsyslog* systemd* apparmor* vim-* xxd xkb-data os-prober discover* dmidecode cryptsetup* laptop-detect kbd keyboard-configuration tasksel*; do
+	apt -y purge ${pkg}
+done
+#apt -y purge rsyslog* systemd* apparmor*
+apt -y autoremove --purge
+rm -rf /etc/boot.d/autoclean; reboot
+EOF
+chmod +x /etc/boot.d/autoclean
+
+sed '/^[23456789]:/s/^/#/' -i /etc/inittab
+reboot
